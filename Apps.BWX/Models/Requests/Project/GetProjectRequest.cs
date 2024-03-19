d1095b6efe016889d92981781492950c83d3660d
bohdanm-bb <bohdan.makarenko@blackbird.io>
2024-03-19 14:50:45 +0200

    Project dynamic input

--- Apps.BWX/Models/Requests/Project/GetProjectRequest.cs
+++ Apps.BWX/Models/Requests/Project/GetProjectRequest.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Apps.BWX.DataSourceHandlers;
+using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Dynamic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +11,8 @@ namespace Apps.BWX.Models.Requests.Project
 {
     public class GetProjectRequest
     {
+        [Display("Project ID")]
+        [DataSource(typeof(ProjectDataHandler))]
         public string ProjectId { get; set; }
     }
 }
