87d70871a20f45639e6b21e68b654a7eadba4f65
bZverok <137277669+bZverok@users.noreply.github.com>
2024-04-09 18:01:21 +0300

    added categories

--- Apps.BWX/BWXApplication.cs
+++ Apps.BWX/BWXApplication.cs
@@ -1,11 +1,18 @@
 ﻿using Blackbird.Applications.Sdk.Common;
 using Blackbird.Applications.Sdk.Common.Authentication.OAuth2;
 using Blackbird.Applications.Sdk.Common.Invocation;
+using Blackbird.Applications.Sdk.Common.Metadata;
 
 namespace Apps.BWX;
 
-public class BWXApplication : IApplication
+public class BWXApplication : IApplication, ICategoryProvider
 {
+    public IEnumerable<ApplicationCategory> Categories
+    {
+        get => [ApplicationCategory.CatAndTms];
+        set { }
+    }
+    
     public string Name
     {
         get => "BWX";
