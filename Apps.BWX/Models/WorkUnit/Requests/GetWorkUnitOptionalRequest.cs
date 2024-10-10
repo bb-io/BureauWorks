f1e5e5c9c73c519c2b912b9df3565fa511068115
vitalii-bezuhlyi <vitalii.bezuhlyi@blackbird.io>
2024-10-10 11:39:52 +0300

    .\Apps.BWX\

---
+++ Apps.BWX/Models/WorkUnit/Requests/GetWorkUnitOptionalRequest.cs
@@ -0,0 +1,9 @@
+﻿using Blackbird.Applications.Sdk.Common;
+
+namespace Apps.BWX.Models.WorkUnit.Requests;
+
+public class GetWorkUnitOptionalRequest
+{
+    [Display("Work unit ID")]
+    public string? WorkUnitId { get; set; }
+}
