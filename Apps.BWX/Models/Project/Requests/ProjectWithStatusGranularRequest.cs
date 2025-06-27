24455cc26223aa22e5513d2ee5d481d937fefa63
Alex Terehov <terales@users.noreply.github.com>
2025-06-27 09:08:55 -0230

    Add 'On projects status changed' polling event (#23)

---
+++ Apps.BWX/Models/Project/Requests/ProjectWithStatusGranularRequest.cs
@@ -0,0 +1,22 @@
+﻿using Apps.BWX.DataSourceHandlers;
+using Apps.BWX.DataSourceHandlers.EnumDataHandlers;
+using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Dictionaries;
+using Blackbird.Applications.Sdk.Common.Dynamic;
+
+namespace Apps.BWX.Models.Project.Requests;
+
+public class ProjectWithStatusGranularRequest
+{
+    [Display("Status"), StaticDataSource(typeof(ProjectStatusDataHandler))]
+    public IEnumerable<string> Statuses { get; set; } = new List<string>();
+
+    [Display("Project ID"), DataSource(typeof(ProjectDataHandler))]
+    public string? ProjectId { get; set; }
+
+    [Display("Reference")]
+    public string? Reference { get; set; }
+
+    [Display("Tags")]
+    public IEnumerable<string>? Tags { get; set; }
+}
