3dd9584f7722e01cc183340f9cb969d578a5636d
ce-nist <cecilia.nistal@blackbird.io>
2025-12-05 22:04:36 -0300

    Added Export project as Loc kit action

---
+++ Apps.BWX/Models/Project/Requests/ExportLocKitRequest.cs
@@ -0,0 +1,15 @@
+﻿using Blackbird.Applications.Sdk.Common;
+
+namespace Apps.BWX.Models.Project.Requests;
+
+public class ExportLocKitRequest
+{
+    [Display("LOC kit type (XLIFF or XLSX)")]
+    public string LocKitTypeJob { get; set; }
+
+    [Display("Work unit UUIDs")]
+    public IEnumerable<string>? WorkUnitUuids { get; set; }
+
+    [Display("Workflow level")]
+    public int WorkflowLevel { get; set; }
+}
---
+++ Apps.BWX/Models/Project/Responses/LocKitDownloadStatusResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace Apps.BWX.Models.Project.Responses;
+
+public class LocKitDownloadStatusResponse
+{
+    public string Id { get; set; }
+    public string Status { get; set; }
+    public string DownloadFileUrl { get; set; }
+    public long StartTime { get; set; }
+    public long EndTime { get; set; }
+    public long CreationDate { get; set; }
+    public string Type { get; set; }
+}
